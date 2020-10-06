d9a8166362a7aea71e63049382ed7a50ca702f84
HappyMakadiya <happysm6343@gmail.com>
2020-10-06 14:38:07 +0530

    commit

--- src/components/NewsCards/Instruction.js
+++ src/components/NewsCards/Instruction.js
@@ -4,7 +4,7 @@ import {Grid} from "@material-ui/core";
 
 const infoCards = [
     { color: '#00838f', title: 'News by Top-Headline',info:'Latest News', text: 'Give me the latest news' },
-    { color: '#1565c0', title: 'News by Categories', info: 'Entertainment, Health, Science, Sports, Technology', text: 'Tell me the recent Technology news' },
+    { color: '#1565c0', title: 'News by Categories', info: 'Entertainment, Health, Science, Sports, Technology', text: 'Tell me the recent news on Technology ' },
     { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
     { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
 ];
