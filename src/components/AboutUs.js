c5110f12aa22c4745b9403687d96ded904f28225
HappyMakadiya <happysm6343@gmail.com>
2020-10-19 10:49:44 +0530

    commit

---
+++ src/components/AboutUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+class AboutUS extends React.Component{
+    render() {
+        return(
+            <div>
+                Hello I am Happy Makadiya
+            </div>
+        )
+    }
+}
+export default AboutUS;
